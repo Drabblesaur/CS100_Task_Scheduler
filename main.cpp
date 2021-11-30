0022157272a864b27a72ef852761fe4ddc0bf2ea
lorenzog1 <lgonz173@ucr.edu>
2021-11-29 23:48:24 -0800

    adding stuff to the main

--- main.cpp
+++ main.cpp
@@ -10,4 +10,27 @@
 
 using namespace std;
 
+vector <Base *>proj;
+vector <Base *>subt;
+vector <base *>tasks; 
+char option; 
 
+cout << "Welcome to the Task Scheduler!"<<endl;
+
+while (option != 'q' || option != 'Q'){
+cout << "Select an option from the Menu:"<<endl;
+cout << "A - Add A Task"<<endl;
+cout << "B - Add A Subtask"<<endl; 
+cout << "C - Mark Task As Completed"<<endl;
+cout << "D - Create A Project"<<endl; 
+cout << "E - Mark Project As Completed"<<endl;
+cout << "F - View Tasks"<<endl;
+cout << "G - View Projects"<<endl; 
+
+cin >> option;
+cout << endl;
+
+if (option =='A' || option == 'a'){
+
+
+}
