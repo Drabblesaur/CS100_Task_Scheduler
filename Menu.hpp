dcd5cec2b07e8c5526ff877ab3fdb1f9cb873069
lorenzog1 <lgonz173@ucr.edu>
2021-11-29 22:14:05 -0800

    created menu implimentations

---
+++ Menu.hpp
@@ -0,0 +1,17 @@
+#ifndef _MENU_HPP_
+#define _MENU_HPP_
+
+#include "Base.hpp"
+#include "Date.hpp"
+#include "task.hpp"
+#include "subtask.hpp"
+#include "project.hpp"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+#endif
