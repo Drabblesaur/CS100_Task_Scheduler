cf0c71049a31d0dc9b3c7f479bd6b0001d394f72
lorenzog1 <lgonz173@ucr.edu>
2021-11-30 01:04:35 -0800

    added more stuff

--- main.cpp
+++ main.cpp
@@ -15,9 +15,8 @@ vector <Base *>subt;
 vector <base *>tasks; 
 char option; 
 
-cout << "Welcome to the Task Scheduler!"<<endl;
 
-while (option != 'q' || option != 'Q'){
+void printMenu(){
 cout << "Select an option from the Menu:"<<endl;
 cout << "A - Add A Task"<<endl;
 cout << "B - Add A Subtask"<<endl; 
@@ -25,12 +24,57 @@ cout << "C - Mark Task As Completed"<<endl;
 cout << "D - Create A Project"<<endl; 
 cout << "E - Mark Project As Completed"<<endl;
 cout << "F - View Tasks"<<endl;
-cout << "G - View Projects"<<endl; 
+cout << "G - View Projects"<<endl;
+}
 
+Base* usertask(){
+string name;
+string date; 
+string description;
+int priority;
+
+cout<<"Enter name of task:"<<endl;
+getline(cin, name);
+
+cout << "Enter task descrition:" <<endl;
+getline(cin,description);
+
+cout<<"Enter task due date:"<<endl;
+getline(cin,date);
+
+cout<<"Enter the level of task priority from a level of 1-5 (1 being low priority, 5 being the most urgent)"<<endl;
+cin >> priority; 
+
+Base* Task = new task(name, description, date, priority);
+return Task;  
+}
+
+Base* userProj(){
+string name; 
+string description;
+} 
+
+int main(){
+cout<<"Welcome to the Task Scheduler"<<endl;
+while(option != 'q' || option != 'Q'){ 
+printMenu();
 cin >> option;
 cout << endl;
 
 if (option =='A' || option == 'a'){
+Base* Task = usertask();
+tasks.push_back(Task);
+}
 
+else if (option == 'B' || option =='b'){
 
+}
+else if(option =='C' || option == 'c'){
+
+}
+
+else if(option == 'D' || option == 'd'){
+Base* Proj = userProj();
+proj.push_back(Proj);
+}
 }
